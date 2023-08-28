84bb5715feeb9500f7501046d45e441739603938
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-08-28 11:31:22 +1000

    Add some extra flags for the cLI

--- src/spew/__init__.py
+++ src/spew/__init__.py
@@ -2,4 +2,4 @@
 Spew - A generator for Python. It uses Python to generate valid Python code.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
--- src/spew/__main__.py
+++ src/spew/__main__.py
@@ -9,13 +9,20 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--depth", type=int, default=4)
+parser.add_argument(
+    "--depth", type=int, default=4, help="Maximum depth (nesting) of the module"
+)
 parser.add_argument("--width", type=int, default=10)
 parser.add_argument("--log-level", type=str, default="INFO")
 parser.add_argument(
-    "--output", type=argparse.FileType("w", encoding="utf-8"), default=None
+    "--output",
+    type=argparse.FileType("w", encoding="utf-8"),
+    default=None,
+    help="Output file. If not specified, the output will be printed to the console.",
 )
-
+parser.add_argument(
+    "--check", action="store_true", help="Check if the code is valid Python"
+)
 args = parser.parse_args()
 
 console = Console()
@@ -25,10 +32,14 @@ m = spew.generate.generate_module(
     depth=args.depth, width=args.width, log_level=args.log_level
 )
 code = ast.unparse(m)
-ast.parse(code, "test.py")
+
 
 if args.output:
     args.output.write(code)
 else:
     syntax = Syntax(code, "python")
     console.print(syntax)
+
+if args.check:
+    ast.parse(code, "test.py")
+    logger.info("Code is valid Python")
